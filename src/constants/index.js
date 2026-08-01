f8cf73bef5994db8304336a677cec1de7c2af5d2
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-08-01 17:55:15 +0330

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const BUSINESS_NAME = 'باغ بادام صادراتی محمد فاضل';
+export const PHONE = '00989010040035';
+export const PHONE_DISPLAY = '۰۰۹۸۹۰۱۰۰۴۰۰۳۵';
+export const MAP_LINK = 'https://maps.app.goo.gl/CEVLpnCoBDTHPzhT7';
+
+export const NAV_ITEMS = [
+  { name: 'خانه', path: '/' },
+  { name: 'درباره ما', path: '/about' },
+  { name: 'خدمات', path: '/services' },
+  { name: 'تماس', path: '/contact' },
+];
